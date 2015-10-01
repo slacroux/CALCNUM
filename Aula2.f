eea4d365f37834ca374550da6839ce99d56276f1
aluno <aluno@localhost.localdomain>
2015-10-01 18:34:30 -0300

    Inicio aulaDois

---
+++ Aula2.f
@@ -0,0 +1,11 @@
+program aulaUm
+
+c Media, variancia e desvio-padrão de um conjunto de dados
+c
+      REAL*8, m, x, var, d
+      Integer n
+      OPEN (1, FILE = 'dados.dat', status = 'unknown')
+c
+c	Entrada
+
+
